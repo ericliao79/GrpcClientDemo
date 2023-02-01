807c81bb6464610744cf8a01a284a4ff488d9599
Eric <eric_liao@owlting.com>
2023-02-01 04:32:05 +0000

    💡 feat: project init

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using GrpcClientDemo.Models;
+using Grpc.Net.Client;
+
+namespace GrpcClientDemo.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+          return View();
+        // var channel = GrpcChannel.ForAddress("http://localhost:5171");
+        // var client = new GrpcGreeterClient.Greeter.GreeterClient(channel);
+        // var result = client.SayHello(new GrpcGreeterClient.HelloRequest { Name = "paynow" });
+
+        //  return Json(new {foo="bar", baz=result.Message});
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
